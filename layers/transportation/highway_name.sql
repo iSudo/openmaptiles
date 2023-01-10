a8a318cd90ea5649dfab2f9cd730741588cd33e3
Erkki Halinen <erkki-eemeli.halinen@cgi.com>
2023-01-10 14:16:25 +0200

    Add city squares POI layer, change leftover name:de to name:sv

--- layers/transportation/highway_name.sql
+++ layers/transportation/highway_name.sql
@@ -4,7 +4,7 @@ SELECT hstore(string_agg(nullif(slice_language_tags(tags ||
                      hstore(ARRAY [
                        'name',    CASE WHEN length(name) > 15    THEN osml10n_street_abbrev_all(name)   ELSE NULLIF(name, '') END,
                        'name:en', CASE WHEN length(name_en) > 15 THEN osml10n_street_abbrev_en(name_en) ELSE NULLIF(name_en, '') END,
-                       'name:de', CASE WHEN length(name_sv) > 15 THEN osml10n_street_abbrev_de(name_sv) ELSE NULLIF(name_sv, '') END
+                       'name:sv', CASE WHEN length(name_sv) > 15 THEN osml10n_street_abbrev_de(name_sv) ELSE NULLIF(name_sv, '') END
                      ]))::text,
                      ''), ','));
 $$ LANGUAGE SQL IMMUTABLE
