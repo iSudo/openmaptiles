2cc9bcb0c5570ab7f90cffcf9e7580e051b4f169
Martin Tõnts <martin@dolmit.com>
2025-09-10 14:06:09 +0300

    est lat map

--- run.sh
+++ run.sh
@@ -3,25 +3,21 @@ set -e
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-export AZURE_STORAGE_ACCOUNT=hslstoragekarttatuotanto
-
-export CONTAINER_NAME=openmaptiles
-export BLOB_NAME=tiles.mbtiles
 export FILENAME=data/tiles.mbtiles
 export PREVIOUS_EXPORT_FILENAME=data/prev/old_tiles.mbtiles
-export MIN_SIZE=660000000
+export MIN_SIZE=150000000
 
 if [ -f $FILENAME ]; then
     mkdir -p data/prev
     mv -f $FILENAME $PREVIOUS_EXPORT_FILENAME
 fi
 
-curl -sSfL "https://karttapalvelu.storage.hsldev.com/finland.osm/finland.osm.pbf" -o data/finland-latest.osm.pbf
 curl -sSfL "https://download.geofabrik.de/europe/estonia-latest.osm.pbf" -o data/estonia-latest.osm.pbf
+curl -sSfL "https://download.geofabrik.de/europe/latvia-latest.osm.pbf" -o data/latvia-latest.osm.pbf
 
-osmium merge data/finland-latest.osm.pbf data/estonia-latest.osm.pbf -o data/fin-est-latest.osm.pbf --overwrite
+osmium merge data/estonia-latest.osm.pbf data/latvia-latest.osm.pbf -o data/est-lat-latest.osm.pbf --overwrite
 
-./quickstart.sh --empty fin-est-latest
+./quickstart.sh --empty est-lat-latest
 
 if [ ! -f $FILENAME ]; then
     (echo >&2 "File not found, exiting")
@@ -32,14 +28,4 @@ if [ $(wc -c <"$FILENAME") -lt $MIN_SIZE ]; then
     (echo >&2 "File size under minimum, exiting")
     exit 1
 fi
-
-if [ -z "$AZURE_BLOB_SAS_ACCESS_KEY" ]; then
-    (echo >&2 "\$AZURE_BLOB_SAS_ACCESS_KEY is empty. Cannot upload mbtiles to Blob, exiting")
-    exit 1
-fi
-
-URL="https://"$AZURE_STORAGE_ACCOUNT".blob.core.windows.net/"$CONTAINER_NAME"/"$BLOB_NAME""
-URL_WITH_SAS=$URL"?"$AZURE_BLOB_SAS_ACCESS_KEY
-echo "Uploading... to " $URL
-azcopy copy $FILENAME $URL_WITH_SAS
 echo "Done."
